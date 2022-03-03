04ec22b554457dbafc7afe14bdf00ae7f45431e8
Akshay <akshayghadavale25@gmail.com>
2022-03-03 14:44:10 +0530

    f1

---
+++ script.js
@@ -0,0 +1,14 @@
+function changec(){
+   
+  let card=document.getElementById("cir1");
+  card.classList.toggle("mystyle");
+ 
+    let card1=document.getElementById("cir");
+ card1.classList.toggle("mystyle2");
+    let card3=document.getElementById("moux");
+ card3.classList.toggle("mou2");
+    let card4=document.getElementById("starsx");
+ card4.classList.toggle("stars2");
+
+
+}
